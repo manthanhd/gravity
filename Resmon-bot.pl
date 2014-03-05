e40e71ed57eaae1edce733478a2b7cb859d2910b
Manthan Dave <manthanhd@live.com>
2014-03-05 14:35:56 +0000

    Major changes to resource monitor bot script. Now gets cpu and memory use from top and disk info from df commands using regex. Also outputs information in json format.
    Former-commit-id: 2aeff56cc388e06c2b774ff523653388754c3be6

--- Resmon-bot.pl
+++ Resmon-bot.pl
@@ -7,11 +7,12 @@
 
 # Prepare. First of all, cleanup.
 use POSIX qw/strftime/;
+use Sys::Hostname;
 
 my $dir = '/scripts/resmon';
 my $memFile = $dir . '/mem.stat';
 my $stop = undef;
-
+my $host = hostname; 
 unlink($memFile);
 
 while(!$stop){
@@ -28,22 +29,27 @@ while(!$stop){
 	}
 	closedir (DIR);
 	
-	my $MemFree = `cat /proc/meminfo | grep MemFree`;
-	if($MemFree =~ /^*(\d+)/){
-			$MemFree = $1;
-	}
-
-	my $MemTotal = `cat /proc/meminfo | grep MemTotal`;
-	if($MemTotal =~ /^*(\d+)/){
-			$MemTotal = $1;
-	}
-	my $MemUse = $MemTotal - $MemFree;
-	
+#	my $MemFree = `cat /proc/meminfo | grep MemFree`;
+#	if($MemFree =~ /^*(\d+)/){
+#			$MemFree = $1;
+#	}
+	#
+#	my $MemTotal = `cat /proc/meminfo | grep MemTotal`;
+#	if($MemTotal =~ /^*(\d+)/){
+#			$MemTotal = $1;
+#	}
+#	my $MemUse = $MemTotal - $MemFree;
+#	
 	# Time to write data in a file in CSV format.
+
+	my ($tasks, $running, $sleeping, $cpu, $memTotal, $memUsed) = analyseUse();
 
 	open (MEMSTAT, ">>$memFile");
 	my $timestamp = strftime('%d-%m-%Y %H:%M:%S',localtime);
-	print MEMSTAT $timestamp . ',' . $MemTotal . ',' . $MemUse . "\n";
+ 	my $diskUseJson = analyseDisk();
+	my $jsonString = "{\"hostname\":\"$host\", \"timestamp\":\"$timestamp\", \"tasksTotal\":\"$tasks\", \"tasksRunning\":\"$running\"," . 
+											"\"tasksSleeping\":\"$sleeping\", \"cpu\":\"$cpu\", \"memTotal\":\"$memTotal\", \"memUsed\":\"$memUsed\", \"diskUse\": $diskUseJson }";
+	print MEMSTAT $jsonString . "\n";
 	close(MEMSTAT);
 	sleep(1);
 }
@@ -52,3 +58,25 @@ sub display {
 	my $str = shift;
 	print $str . "\n";
 }
+
+sub analyseUse {
+	my $txt = `top -n 1`;
+	$regex = '.*?Tasks.*?(\\d+)\\s.*?total.*?(\\d+)\\s.*?running.*?(\\d+)\\s.*?sleeping.*?Cpu.*?([+-]?\\d*\\.\\d+)(?![-+0-9\\.]).*?Mem.*?(\\d+)k.*?(\\d+)k.*?';
+	my ($tasks, $running, $sleeping, $cpu, $memTotal, $memUsed) = $txt =~ m/$regex/is;
+	# print "\nTasks:$tasks, Running $running, Sleeping $sleeping, Cpu: $cpu, memTotal:$memTotal, memFree:$memUsed";
+	return ($tasks, $running, $sleeping, $cpu, $memTotal, $memUsed);
+}
+
+sub analyseDisk {
+	my $txt = `df -h`;
+  my $regex = '^.*?sda1\\s+(\\d+[.\\d]+)(.)\\s+(\\d+[.\\d]+)(.).*?tmpfs\\s+(\\d+[.\\d]+)(.)\\s+(\\d+[.\\d]+)(.)';
+	my ($rootSize, $rootSizeUnit, $rootUsed, $rootUsedUnit, $tmpSize, $tmpSizeUnit, $tmpUsed, $tmpUsedUnit) = $txt =~ m/$regex/is;
+  my $jsonString = "{ \"rootSize\":{ \"value\":\"$rootSize\", \"unit\":\"$rootSizeUnit\" }, " .
+					"\"rootUsed\":{\"value\":\"$rootUsed\", \"unit\":\"$rootUsedUnit\"}, " .
+          "\"tmpSize\":{\"value\":\"$tmpSize\", \"unit\":\"$tmpSizeUnit\"}, " .
+          "\"tmpUsed\":{\"value\":\"$tmpUsed\", \"unit\":\"$tmpUsedUnit\"} }";
+					# print "\nSize of root partition: $rootSize $rootSizeUnit, Used: $rootUsed $rootUsedUnit\n";
+					# print "\nSize of tmp partition: $tmpSize $tmpSizeUnit, Used: $tmpUsed $tmpUsedUnit\n";
+	return $jsonString;
+}
+
